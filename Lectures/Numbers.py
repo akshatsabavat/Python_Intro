bd9cc40923a703db40e58a2c5aadcf494bd5d762
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-12 12:07:54 +0530

    feat(Lectures --Numbers) - Code added #2

--- Lectures/Numbers.py
+++ Lectures/Numbers.py
@@ -1,3 +1,5 @@
+import math # library that contains mathematical computation functions
+
 x = 10 # normal integer
 y = 0b10 # checks the binary code and returns integer (2)
 z = 0b110 # returns 6
@@ -5,3 +7,7 @@ print(x,y,z)
 
 w = 2 + 7j # complex number
 print(w)
+
+marks_Science = 91.5
+print(round(marks_Science)) # not included in math library/module
+print(math.floor(marks_Science)) # included in math library/module

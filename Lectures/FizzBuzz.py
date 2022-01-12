fd7eafe4c5efc445930a9ffbb4542234d6739c73
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-12 16:28:28 +0530

    feat(Lectures --FizzBuzz) - Code added

---
+++ Lectures/FizzBuzz.py
@@ -0,0 +1,12 @@
+def fizz_buzz(user_input):
+    if (user_input % 3 == 0) and (user_input % 5 == 0):
+        print("FizzBuzz")
+    elif user_input % 3 == 0:
+        print("Fizz")
+    elif user_input % 5 == 0:
+        print("Buzz")
+    else:
+        print(user_input)
+
+
+fizz_buzz(15)
--- Lectures/Function.py
+++ Lectures/Function.py
@@ -29,3 +29,17 @@ print(Multiply(2,5))
 
 #Making code more readable
 print(Multiply(number = 2, by = 3))
+
+#Argument techniques
+def multiply(*list): #Lets Python know that u wanna pass in a tuple
+    total = 1
+    for number in list:
+        total *= number
+    return total
+
+print(multiply(2,3,4,5,2,1))
+
+def save_user(**user): #lets us pass multiple different object types
+    print(user)
+
+save_user(id = 2010110063, name = "Akshat Sabavat") # returns a object like in javascript

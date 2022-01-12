dfc37e5e93b697585cd7ddbdcefbd29d519dba4a
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-12 11:36:41 +0530

    feat(Lectures --TypeAnnotation) - Code added #2

--- Lectures/TypeAnnotation.py
+++ Lectures/TypeAnnotation.py
@@ -1,3 +1,3 @@
-age: int = 20
-age = "python"
+age: int = 20 # specifies the variable type and mypy linter detects if its changed or not
+age = "python" #my py detects this change and warns us
 print(age)

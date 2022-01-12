8b91b20d1ed4bb8726108150b5b281288c38f922
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-12 15:29:18 +0530

    feat(Lectures --Loops #Forloops) - File added

--- Lectures/Loops.py
+++ Lectures/Loops.py
@@ -1,1 +1,10 @@
 #loop syntax stays very differnt in python as its all about indentation
+for x in "Python" :
+    print(x) # prints the letters one by one
+
+for q in ['a','b','c']:
+    print(q) # prints all the elements of the array
+
+for x in range(0,10,2): #range function has 3 arguments start no, end no and step if step is 2 it prints even numbers
+    print(x) # range doesnt return a list
+

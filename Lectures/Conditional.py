0c776bbbf7e6af9a5a3e9227dd5dc0838bcaf7c0
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-12 12:31:18 +0530

    feat(Lectures --Conditional) - Code added #2

--- Lectures/Conditional.py
+++ Lectures/Conditional.py
@@ -1,5 +1,7 @@
 boy_age = 22
 
+# conditional statements dont have () or {} instead code blocks are specified by : 
+
 if boy_age >= 18:
      print("Adult")
 elif boy_age >= 13:
@@ -9,4 +11,12 @@ else:
 
 print("All done")
 
-# conditional statements dont have () or {} instead code blocks are specified by : 
+name = "Box"
+Namey = ""
+
+if not Namey: #condition to check if the Namey is empty or not
+        print("Namey is empty")
+else:
+       print("Namey is not empty")
+
+#Ternary Operator syntax stays the same

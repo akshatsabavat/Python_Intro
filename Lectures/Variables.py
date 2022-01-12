dfd14317b67e184b6a00e63be46572c133273277
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-12 11:25:59 +0530

    feat(Lectures --Variables) - Code added

---
+++ Lectures/Variables.py
@@ -0,0 +1,7 @@
+college_students = 1000 # integer
+rating = 5.1 # float
+is_published = False # boolean
+course_name = "English" # one lined string 
+course_description = """This a universally  
+                     Spoken Language""" # multi lined string
+x, y = 1, 6 # Same as x = 1 and y = 6

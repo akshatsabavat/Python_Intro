f2cf55781dce263ff0707f8460f17f484d746f34
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-12 12:27:03 +0530

    feat(Lectures --Conditional) - Code added

---
+++ Lectures/Conditional.py
@@ -0,0 +1,12 @@
+boy_age = 22
+
+if boy_age >= 18:
+     print("Adult")
+elif boy_age >= 13:
+    print("Teenager")
+else:
+    print("Child")
+
+print("All done")
+
+# conditional statements dont have () or {} instead code blocks are specified by : 
